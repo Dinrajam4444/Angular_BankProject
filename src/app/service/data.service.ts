a1fa78bfe8c445c1149e91ab38ed5cca81b66709
Dinraj AM <dinrajrockzz444@gmail.com>
2023-08-01 15:05:15 +0530

    fifth commit Done till service file section of my angular bank project

---
+++ src/app/service/data.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  sData="Data inside service file"
+
+  constructor() { }
+
+  accessData(data:any){
+    console.log(data);
+    
+  }
+}
