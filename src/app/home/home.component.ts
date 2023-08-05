9f190bbd674f4d7592e16bd3e3f5a5fe680429a3
Dinraj AM <dinrajrockzz444@gmail.com>
2023-08-06 03:33:52 +0530

    eighth commit of server of angular bank project

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from '@angular/router';
+import { DataService } from '../service/data.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -8,8 +9,10 @@ import {Router} from '@angular/router';
 export class HomeComponent implements OnInit {
 
   user:any=""
+  acno:any
+  profileData:any={}
 
-  constructor(private rout:Router) { }
+  constructor(private rout:Router,private ds:DataService ) { }
 
   ngOnInit(): void {
     if (localStorage.getItem("currentUname")) {
@@ -24,4 +27,19 @@ export class HomeComponent implements OnInit {
   logout(){
     this.rout.navigateByUrl("")
   }
+
+  profileView(){
+    if(localStorage.getItem("currentAcno")){
+      this.acno= localStorage.getItem("currentAcno")
+      // console.log(this.acno);
+      
+    }
+    this.ds.getProfile(this.acno).subscribe((response:any)=>{
+      // console.log(response);
+      this.profileData=response
+      
+    })
+
+  }
+
 }
